85231fba87b5b4736df0a8a2d2f021eb1ad7f1c5
Ahmed Alazawy <aalazawy625@gmail.com>
2021-04-27 11:50:10 +0100

    first commit

---
+++ java/src/C19316336/AhmedsVisual.java
@@ -0,0 +1,18 @@
+package C19316336;
+
+import ie.tudublin.Visual;
+import ie.tudublin.VisualException;
+
+public class AhmedsVisual extends Visual {
+
+    float specLow = 0.03f, specMid = 0.125f, specHi = 0.20f;
+    float minScore = 0, avgScore = 0, maxScore = 0;
+    float minScore1 = minScore, avgScore1 = avgScore, maxScore1 = maxScore;
+    float scoreDecreaseRate = 25;
+    int noOfBgBoxes = 500;
+    BackgroundBoxes[] backgroundBoxes;
+
+    public void settings()
+    {
+        fullScreen(P3D, SPAN);
+    }
